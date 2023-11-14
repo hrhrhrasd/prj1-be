e47e2341ca2d88385e6d61abf0605ce6845e0e14
hrhrhrasd <gmleh1245@gmail.com>
2023-11-14 09:43:48 +0900

    로그인 후 Member객체 session attribute "login" 추가

--- src/main/java/com/example/prj1be/controller/MemberController.java
+++ src/main/java/com/example/prj1be/controller/MemberController.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Delete;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 
@@ -91,8 +92,8 @@ public class MemberController {
     }
 
     @PostMapping("login")
-    public ResponseEntity login(@RequestBody Member member) {
-        if (service.login(member)) {
+    public ResponseEntity login(@RequestBody Member member, WebRequest request) {
+        if (service.login(member, request)) {
             return ResponseEntity.ok().build();
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
--- src/main/java/com/example/prj1be/service/MemberService.java
+++ src/main/java/com/example/prj1be/service/MemberService.java
@@ -4,6 +4,8 @@ import com.example.prj1be.domain.Member;
 import com.example.prj1be.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 
@@ -75,10 +77,12 @@ public class MemberService {
         return mapper.update(member) == 1;
     }
 
-    public boolean login(Member member) {
+    public boolean login(Member member, WebRequest request) {
         Member dbMember = mapper.selectById(member.getId());
         if (member != null) {
             if (dbMember.getPassword().equals(member.getPassword())) {
+                dbMember.setPassword("");
+                request.setAttribute("login", dbMember, RequestAttributes.SCOPE_SESSION);
                 return true;
             }
         }
